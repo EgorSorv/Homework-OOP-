cb319b8aa692c923f8e03952082da6f04959a41a
Egor Sorokin <egor.v.s@mail.ru>
2023-04-16 21:38:43 +0300

    errors fixed

--- src/main/java/org/hw/hw3/Product/Water.java
+++
@@ -1,7 +0,0 @@
-package org.hw.hw3.Product;
-
-public class Water extends Drink {
-    public Water(String name, Double price, Double volume) {
-        super(name, price, volume);
-    }
-}
--- src/main/java/org/hw/hw3/VendingMachine/VendingMachine.java
+++
@@ -1,9 +0,0 @@
-package org.hw.hw3.VendingMachine;
-
-import org.hw.hw3.Product.Product;
-
-import java.util.TreeSet;
-
-public interface VendingMachine {
-    TreeSet<Product> getProduct (String name);
-}
